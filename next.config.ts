802b80ccb7d66f68587f0730bf53b4557e8a07eb
abdul-abdi <Abdullahiabdi1233@gmail.com>
2025-04-09 17:26:54 +0300

    Enhance Next.js configuration with additional settings
    - Enabled compression for improved performance.
    - Disabled the default powered by header for security.
    - Activated React strict mode for better error handling.
    - Configured remote image patterns to allow images from any HTTPS source.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  compress: true,
+  poweredByHeader: false,
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
